5d2ee560d5683db5d4e036e334b25f7b66e7bdc1
Nan Monnand Deng <monnand@gmail.com>
2013-08-28 16:18:50 -0400

    autoscale.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime"
 )
 
 var argvHTTPPort = flag.Int("port", 8080, "HTTP service port number")
@@ -12,6 +13,7 @@ var argvNrServers = flag.Int("n", 1, "number of servers")
 
 func main() {
 	flag.Parse()
+	runtime.GOMAXPROCS(runtime.NumCPU() + 2)
 	r := NewRecursiveDelay(*argvNrServers)
 	addr := fmt.Sprintf("0.0.0.0:%v", *argvHTTPPort)
 	log.Fatal(http.ListenAndServe(addr, r))
--- recurdelay.go
+++ recurdelay.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
@@ -20,34 +21,81 @@ type request struct {
 }
 
 type RecursiveDelay struct {
-	reqChan chan *request
+	reqChan   chan *request
+	stopChan  chan bool
+	autoScale bool
+	nrServers int32
 }
 
 func NewRecursiveDelay(nrServers int) *RecursiveDelay {
 	ret := new(RecursiveDelay)
 	ret.reqChan = make(chan *request)
+	ret.stopChan = make(chan bool)
 	if nrServers <= 0 {
 		nrServers = 1
+		ret.autoScale = true
 	}
-	for i := 0; i < nrServers; i++ {
-		go ret.serveRequest()
+
+	// We should always make sure there is
+	// at least one server.
+	go ret.serveRequest(nil)
+	for i := 0; i < nrServers-1; i++ {
+		go ret.serveRequest(ret.stopChan)
 	}
 	return ret
 }
 
-func (self *RecursiveDelay) serveRequest() {
-	for req := range self.reqChan {
-		res := new(response)
-		res.start = time.Now()
-		req.path.Delay()
-		res.end = time.Now()
-		res.err = nil
-		req.respChan <- res
+func (self *RecursiveDelay) serveRequest(stopChan <-chan bool) {
+	atomic.AddInt32(&self.nrServers, int32(1))
+	defer atomic.AddInt32(&self.nrServers, int32(-1))
+	for {
+		select {
+		case req := <-self.reqChan:
+			if req == nil {
+				return
+			}
+			res := new(response)
+			res.start = time.Now()
+			req.path.Delay()
+			res.end = time.Now()
+			res.err = nil
+			req.respChan <- res
+		case <-stopChan:
+			return
+		}
+	}
+}
+
+func (self *RecursiveDelay) scale(queuingDelay, serviceTime time.Duration) {
+	if queuingDelay*2 < serviceTime {
+		// Too small queuing delay, we are wasting resources.
+		self.scaleDown()
+	}
+	if queuingDelay > serviceTime*2 {
+		// No enough resources.
+		self.scaleUp()
+	}
+}
+
+func (self *RecursiveDelay) scaleUp() {
+	go self.serveRequest(self.stopChan)
+}
+
+func (self *RecursiveDelay) scaleDown() {
+	select {
+	case self.stopChan <- true:
+	default:
+		return
 	}
 }
 
 func (self *RecursiveDelay) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
+
+	if r.URL.Path == "/nrservers" {
+		fmt.Fprint(w, "%v\n", atomic.LoadInt32(&self.nrServers))
+		return
+	}
 
 	pathBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -80,7 +128,12 @@ func (self *RecursiveDelay) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "site: %v; queuing delay: %v; service time: %v\n", path.Sites[0].Name(), res.start.Sub(start), res.end.Sub(res.start))
+	qdelay := res.start.Sub(start)
+	stime := res.end.Sub(res.start)
+	fmt.Fprintf(w, "site: %v; queuing delay: %v; service time: %v\n", path.Sites[0].Name(), qdelay, stime)
+	if self.autoScale {
+		self.scale(qdelay, stime)
+	}
 
 	body, err := path.Forward()
 	if err != nil {
